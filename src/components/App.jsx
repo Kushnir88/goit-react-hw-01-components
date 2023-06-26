9f12a1dbca20d5acfef9c7d1413d7ea1cf22af0c
Kushnir88 <mr.kushnir.igor@gmail.com>
2023-06-26 23:29:06 +0300

    update HW

---
+++ src/components/App.jsx
@@ -0,0 +1,30 @@
+import { Profile } from './profile/Profile'
+import { Statistics } from './statistics/Statistics'
+import {FriendList} from './friendList/FriendsList'
+import { TransactionHistory } from './transaction/Transaction'
+
+
+import user from '../data/user.json'
+import dataInf from '../data/data.json'
+import friends from '../data/friends.json'
+import transactions from '../data/transactions.json'
+ const App =() =>{
+    return (
+    <>
+    <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+        
+      />
+      <Statistics title="Upload stats" stats={dataInf} />
+     <FriendList friends={friends} 
+    
+      />
+     <TransactionHistory items={transactions} />;
+      </>)
+      }
+
+export default App;
---
+++ src/components/friendList/FriendsList.jsx
@@ -0,0 +1,16 @@
+import { FriendListItem } from '../friendListItem/FriendListItem'
+import css from './FriendList.module.css'
+export function FriendList({ friends}) {
+    return (
+      <ul className={css.friendList}>
+        {friends.map((friend) => (
+        <FriendListItem 
+        key={friend.id} 
+        avatar={friend.avatar}
+        name={friend.name}
+        isOnline={friend.isOnline}
+        />
+      ))}
+      </ul>
+    );
+  }
